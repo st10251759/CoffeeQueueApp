14bb079cac7cda3dd931eb9f3091e05331f7ccc6
Cameron Chetty <st10251759@myemeris.edu.za>
2026-03-10 22:59:33 +0200

    Linking to SQL Server Database In order to link your MVC App to local SQL Databse you must add code to 3 files: -ApplicationDbContext.cs -appsettings.json -Program.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeQueueApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeQueueApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<CoffeeOrder> CoffeeOrder => Set<CoffeeOrder>();
+        public DbSet<Barista> Baristas => Set<Barista>();
+
+        //public DbSet<Barista> Baristas { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CoffeeQueueApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace CoffeeQueueApp
 {
     public class Program
@@ -8,6 +11,9 @@ namespace CoffeeQueueApp
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
