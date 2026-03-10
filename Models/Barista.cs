ad6febb8b34f733c7b2c346817cdfd4039275ab4
Cameron Chetty <st10251759@myemeris.edu.za>
2026-03-10 22:42:23 +0200

    Added Models

---
+++ Models/Barista.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeQueueApp.Models
+{
+    public class Barista
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Barista Name Required")]
+        [StringLength(50)]
+
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Barista shift time required")]
+        [StringLength(20)]
+
+        public string Shift { get; set; } = "Morning";
+
+        //navigation property/foreign key
+        public List<CoffeeOrder> Order { get; set; } = new List<CoffeeOrder>();
+        // oner order can only have one barsita 
+
+
+    }
+}
---
+++ Models/CoffeeOrder.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeQueueApp.Models
+{
+    public class CoffeeOrder
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Customer Name is required")]
+        [StringLength(50)]
+        public string CustomerName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Drink { get; set; } = "Latte";
+
+        //size
+        [Required]
+        [StringLength(15)]
+        public string Size { get; set; } = "Medium";
+
+        //milk option
+        [Required]
+        [StringLength(50)]
+        public string Milk { get; set; } = "Full Cream";
+
+        //sugar
+        [Range(0, 6, ErrorMessage = "Sugar must be between 0 and 6")]
+        public int Sugar { get; set; } = 0;
+
+        //status of order (pending, in progress, completed)
+        [Required]
+        [StringLength(15)]
+        public string Status { get; set; } = "Completed";
+
+        //date created
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
